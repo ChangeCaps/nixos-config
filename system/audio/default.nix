0427798dae4fe768159e4c500bb95e91e0e62486
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2025-01-09 14:46:53 +0100

    Update audio config more sensibly

--- system/audio/default.nix
+++ system/audio/default.nix
@@ -2,6 +2,8 @@
 
 let
   cfg = config.audio;
+  sample-rate = 48000;
+  quantum = 64;
 in {
   options.audio = {
   };
@@ -28,6 +30,8 @@ in {
       '';
     };
 
+    security.rtkit.enable = true;
+
     # Enable sound with pipewire.
     services.pulseaudio.enable = false;
     services.pipewire = {
@@ -38,17 +42,13 @@ in {
       # If you want to use JACK applications, uncomment this
       jack.enable = true;
 
-      raopOpenFirewall = true;
-
       extraConfig = {
         pipewire."92-low-latency" = {
           "context.properties" = {
-            "default.clock.rate"        = 48000;
-            "default.clock.quantum"     = 32;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 32;
-            "link.max-buffers"          = 4;
-            "link.max-frames"           = 16;
+            "default.clock.rate"        = sample-rate;
+            "default.clock.quantum"     = quantum;
+            "default.clock.min-quantum" = quantum;
+            "default.clock.max-quantum" = quantum;
           };
 
           "context.modules" = [
@@ -64,7 +64,45 @@ in {
             }
           ];
         };
+
+        #pipewire-pulse."92-low-latency" = {
+        #  "context.modules" = [
+        #    {
+        #      name = "libpipewire-module-protocol-pulse";
+        #      args = {
+        #        "pulse.default.req" = quantum-fraction;
+        #        "pulse.min.req"     = quantum-fraction;
+        #        "pulse.max.req"     = quantum-fraction;
+        #        "pulse.min.quantum" = quantum-fraction;
+        #        "pulse.max.quantum" = quantum-fraction;
+        #      };
+        #    }
+        #  ];
+        #
+        #  "stream.properties" = {
+        #    "node.latency" = quantum-fraction;
+        #    "resample.quality" = 1;
+        #  };
+        #};
       };  
     }; 
+
+    services.pipewire.wireplumber.extraConfig."99-alsa-lowlatency" = {
+      "monitor.alsa.rules" = {
+        matches = [
+          {
+            "node.name" = "~alsa_output.*";
+          }
+        ];
+        actions = {
+          update-props = {
+            "audio.format" = "S32LE";
+            "audio.rate" = sample-rate * 2;
+            "api.alsa.period-size" = 2;
+            "api.alsa.disable-batch" = false;
+          };
+        };
+      };
+    };
   };
 }
